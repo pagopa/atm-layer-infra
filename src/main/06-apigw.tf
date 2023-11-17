b35a30179c5b2bf8b061d126592bf921a9ccc38b
smicheleaws <144434184+smicheleaws@users.noreply.github.com>
2023-11-17 16:24:04 +0100

    fix: cognito userpool (#6)

--- src/main/06-apigw.tf
+++ src/main/06-apigw.tf
@@ -226,11 +226,9 @@ resource "aws_cognito_user_pool_client" "client" {
   allowed_oauth_scopes         = aws_cognito_resource_server.resource.scope_identifiers
   supported_identity_providers = ["COGNITO"]
 
-  token_validity_units {
-    access_token  = "minutes"
-    id_token      = "minutes"
-    refresh_token = "days"
-  }
+  refresh_token_validity = 4
+  access_token_validity  = 1
+  id_token_validity      = 1
 }
 
 resource "aws_cognito_user_pool_domain" "domain" {
